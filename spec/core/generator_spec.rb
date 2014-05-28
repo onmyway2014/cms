0e74d942970adee579ad6ca423fd560e314ca4ad
onmyway2014 <onmyway2014@foxmail.com>
2014-05-28 08:54:32 +0800

    refactor :generator_spec

--- spec/core/generator_spec.rb
+++ spec/core/generator_spec.rb
@@ -4,7 +4,30 @@ require './src/core/resource'
 require './src/core/template_engine_factory'
 
 module Cms
+
   describe Generator do
+
+    def create_generator(engine_type, model_data, template_data, render_result)
+      model_r = double 'model_resource'
+      template_r = double 'template_resource'
+      dest_r = double 'destination_resource'
+
+      model_r.should_receive(:get).and_return model_data
+      template_r.should_receive(:get).and_return template_data
+      dest_r.should_receive(:put).with(render_result)
+
+      template_engine = double 'template_engine'
+      template_engine.should_receive(:render).with(model_data, template_data).and_return render_result
+
+      template_engine_factory = TemplateEngineFactory.new
+      template_engine_factory.should_receive(:create).with(engine_type).and_return template_engine
+      TemplateEngineFactory.stub(:new).and_return template_engine_factory
+
+      generator = Generator.new(model_r, template_r, dest_r)
+      generator.engine_type = engine_type
+      generator
+    end
+
 
     it 'raise an exception when init argument given nil' do
       expect { Generator.new(nil, nil, nil) }.to raise_error(ArgumentError, 'model_resource is nil')
@@ -15,28 +38,7 @@ module Cms
     end
 
     it 'run' do
-      model_r = double 'model_resource'
-      template_r = double 'template_resource'
-      dest_r = double 'destination_resource'
-      engine = Generator.new(model_r, template_r, dest_r)
-
-      model_data = 'model data'
-      template_data = 'template data'
-      render_result = 'result'
-
-      model_r.should_receive(:get).and_return model_data
-      template_r.should_receive(:get).and_return template_data
-      dest_r.should_receive(:put).with(render_result)
-
-      template_engine = double 'template_engine'
-      template_engine.should_receive(:render).with(model_data, template_data).and_return render_result
-
-      template_engine_factory = TemplateEngineFactory.new
-      template_engine_factory.should_receive(:engine).and_return template_engine
-      TemplateEngineFactory.stub(:new).and_return template_engine_factory
-
-      engine.run
-      engine.should be_success
+      create_generator('test', 'model data', 'template_data', 'result data').should be_run
     end
 
 
--- src/core/generator.rb
+++ src/core/generator.rb
@@ -5,6 +5,7 @@ module Cms
   class Generator
 
     attr_reader :model_resource, :template_resource, :destination_resource
+    attr_accessor :engine_type
 
     def initialize (model_resource, template_resource, destination_resource)
       raise ArgumentError, 'model_resource is nil' if model_resource.nil?
@@ -15,33 +16,27 @@ module Cms
       @template_resource = template_resource
       @destination_resource = destination_resource
 
-
+      @engine_type = 'default'
     end
 
-    def run
+    # @return [TrueClass,FalseClass]
+    def run?
       model = model_resource.get
-      template = template_resource.get
+      template = template_resource.get.to_s
 
-      template_engine = factory.engine 'eob'
+      template_engine = engine_factory.create @engine_type
 
       result = template_engine.render model, template
 
       destination_resource.put result
 
-      @success = true
-    end
-
-    def success?
-      @success
+      true
     end
 
     private
-    def factory
-      if @template_engine_factory.nil?
-        @template_engine_factory = TemplateEngineFactory.new
-      end
-      @template_engine_factory
-    end
+    def engine_factory
+      @template_engine_factory = TemplateEngineFactory.new if @template_engine_factory.nil?
+    end   
 
   end
 end
